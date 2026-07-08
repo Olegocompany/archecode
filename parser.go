9fdbaf4dc099a1a8c6fc67b324fe4964d26d8c1c
elfi <gugl6369@gmail.com>
2026-07-08 22:02:19 +0700

    add: build parser.so

--- parser.go
+++ parser.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"C"
 	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type DirObject interface {
@@ -20,7 +22,7 @@ type File struct {
 
 type Folder struct {
 	Name     string      `json:"name"`
-	Children []DirObject `json:"children"`
+	Children []DirObject `json:"childrens"`
 }
 
 func (f File) GetName() string   { return f.Name }
@@ -28,11 +30,13 @@ func (f File) IsDir() bool       { return false }
 func (f Folder) GetName() string { return f.Name }
 func (f Folder) IsDir() bool     { return true }
 
-func main() {
+// name - имя проекта (для вывода json ), pathTo - путь до директории парсинга, pathAfter - куда создать json файл
+func parser(name string, pathTo string, pathAfter string) {
+	rootPath := pathTo
 
-	rootPath := "проект"
+	// создание корневой папки
 	root := &Folder{
-		Name:     filepath.Base(rootPath),
+		Name:     name,
 		Children: []DirObject{},
 	}
 
@@ -40,17 +44,25 @@ func main() {
 		rootPath: root,
 	}
 
-	err := filepath.Walk("проект",
+	err := filepath.Walk(pathTo,
 		func(path string, info os.FileInfo, err error) error {
+
+			// проверка на ошибки
 			if err != nil {
 				return err
 			}
 
+			// для того, чтоб не дублировалась корневая папка
 			if rootPath == path {
 				return nil
 			}
 
+			if strings.HasPrefix(info.Name(), ".") {
+				return nil
+			}
+
 			parentDir := filepath.Dir(path)
+
 			parent, ok := foldersMap[parentDir]
 			if !ok {
 				return nil
@@ -59,15 +71,16 @@ func main() {
 			if !info.IsDir() {
 				filePath := filepath.Join(path)
 				content, err := os.ReadFile(filePath)
-				if err != nil {
-					log.Printf("Не удалось прочитать файл по пути %s: %v\n", filePath)
-					return nil
+				if err == nil {
+					file := File{
+						Name:    info.Name(),
+						Content: string(content),
+					}
+					parent.Children = append(parent.Children, file)
+				} else {
+					fmt.Println("файл " + info.Name() + " проблемный")
 				}
-				file := File{
-					Name:    info.Name(),
-					Content: string(content),
-				}
-				parent.Children = append(parent.Children, file)
+
 			} else {
 				folder := &Folder{
 					Name:     info.Name(),
@@ -78,14 +91,26 @@ func main() {
 			}
 			return nil
 		})
+	// проверка на ошибки
 	if err != nil {
 		log.Println(err)
 	}
 
+	createJSON(name, pathAfter, root)
+}
+
+func createJSON(name string, pathAfter string, root *Folder) {
+
 	folderJSON, err := json.MarshalIndent(root, "", "	")
+
+	// проверка на ошибки
 	if err != nil {
-		fmt.Println("Ошибка при конвертации в JSON:", err)
 		return
 	}
-	err = os.WriteFile("data.json", folderJSON, 0644)
+
+	err = os.WriteFile(pathAfter+"/"+name+".json", folderJSON, 0644)
+}
+
+func main() {
+
 }
