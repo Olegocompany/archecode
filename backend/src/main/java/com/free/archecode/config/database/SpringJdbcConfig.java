ac15060b84aef88741176433d2849fe5d17af397
rh64s <cchelovekov64@gmail.com>
2026-07-11 17:29:57 +0700

    refactor: use env for database configuration

--- backend/src/main/java/com/free/archecode/config/database/SpringJdbcConfig.java
+++ backend/src/main/java/com/free/archecode/config/database/SpringJdbcConfig.java
@@ -1,5 +1,6 @@
 package com.free.archecode.config.database;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -10,14 +11,21 @@ import javax.sql.DataSource;
 @Configuration
 @ComponentScan("com.free.archecode")
 public class SpringJdbcConfig {
+
+    @Value("${spring.datasource.url}")
+    private String databaseUrl;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+
     @Bean
     public DataSource mysqlDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3307/db");
-        dataSource.setUsername("root");
-        dataSource.setPassword("password!");
-
+        dataSource.setUrl(databaseUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
         return dataSource;
     }
 }
