dc5b3ad350e1b5f99ee155136cd60e3e1d50bd8a
HABAB0 <tolya.shaprove@mail.ru>
2026-07-13 18:23:58 +0700

    fix

--- frontend/app/shared/ui/form/input/Input.tsx
+++ frontend/app/shared/ui/form/input/Input.tsx
@@ -16,7 +16,7 @@ function Input({ disabled, error, placeholder, type = "text" }: InputProps) {
         placeholder={placeholder}
         type={type}
       />
-      {typeof !!error && <p className="text-sm text-error mt-0.75">{error}</p>}
+      {!!error && <p className="text-sm text-error mt-0.75">{error}</p>}
     </label>
   );
 }
