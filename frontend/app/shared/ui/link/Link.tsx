ebb3bafe290cb2bc666154e4522446d433d0bd6f
HABAB0 <tolya.shaprove@mail.ru>
2026-07-13 18:39:02 +0700

    fix: change link state to NEW GREEN COLOR

--- frontend/app/shared/ui/link/Link.tsx
+++ frontend/app/shared/ui/link/Link.tsx
@@ -8,9 +8,9 @@ interface LinkProps {
 function Link({ to, children }: LinkProps) {
   return (
     <a
-      className="text-accent-light cursor-pointer [text-shadow:0_0_6px_rgba(0,185,6,0.75)]
+      className="text-accent-light cursor-pointer [text-shadow:0_0_6px_rgba(0,185,6,0.75)] transition duration-300
             hover:text-accent-lighter
-            active:[text-shadow:0_0_6px_rgba(0,185,6,0.5)] transition duration-300"
+            active:text-accent-dark active:[text-shadow:0_0_6px_rgba(0,185,6,0.5)]"
       href={to}
     >
       {children}
