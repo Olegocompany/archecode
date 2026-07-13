3ae14c295f4d3a033f5b032b92c9c01d7902690a
lisantov <lisantov@yandex.ru>
2026-07-13 13:53:42 +0700

    refactor: fixed architecture and variables

--- frontend/app/shared/ui/link/Link.tsx
+++ frontend/app/shared/ui/link/Link.tsx
@@ -10,7 +10,7 @@ function AppLink({ to, children }: LinkProps) {
     <a
       className="text-accent-light cursor-pointer [text-shadow:0_0_6px_rgba(0,185,6,0.75)]
             hover:text-accent-lighter
-            active:[text-shadow:0_0_6px_rgba(0,185,6,0.5)]"
+            active:[text-shadow:0_0_6px_rgba(0,185,6,0.5)] transition duration-300"
       href={to}
     >
       {children}
