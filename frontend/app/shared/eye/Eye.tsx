c576eb571269ccf772a013af1102b4fc9f6e73d1
HABAB0 <tolya.shaprove@mail.ru>
2026-07-13 18:28:04 +0700

    fix: change props in comp eye

--- frontend/app/shared/eye/Eye.tsx
+++ frontend/app/shared/eye/Eye.tsx
@@ -1,13 +1,13 @@
 import { CloseEye, OpenEye } from "@/app/shared/svg";
 
 interface EyeProps {
-  state?: "open" | "close";
+  state: boolean;
 }
 
-function Eye({ state = "open" }: EyeProps) {
+function Eye({ state = true }: EyeProps) {
   return (
     <>
-      {state === "open" ? (
+      {state ? (
         <OpenEye className="w-full h-full cursor-pointer text-primary-dark  hover:text-lighter-gray active:text-input-placeholder transition duration-300" />
       ) : (
         <CloseEye className="w-full h-full cursor-pointer text-primary-dark  hover:text-lighter-gray active:text-input-placeholder transition duration-300" />
