005f3e80d0a2ea5740ad0c46df4bba22d4d25d9d
elfi <gugl6369@gmail.com>
2026-07-15 21:30:52 +0700

    add: animation and change text

--- frontend/app/auth/_components/widget.tsx
+++ frontend/app/auth/_components/widget.tsx
@@ -1,23 +1,37 @@
 "use client";
 import {ReactNode, useEffect, useEffectEvent, useRef, useState} from "react";
 
-
 export default function Widget() {
   const [timers, setTimers] = useState(0);
   const [point, setPoint] = useState([true, false, false]);
   const [positionPoint, setPositionPoint] = useState(0);
   const elementPo = useRef<SVGCircleElement>(null)
+  const elementPicture = useRef<HTMLDivElement>(null)
+  const elementBlockPicture = useRef<HTMLDivElement>(null)
   const [progress, setProgress] = useState(1)
+  const [onAnimation, setOnAnimation] = useState(true)
+  const listPicture = ['/widget/photo-one.png', '/widget/photo-two.png', '/widget/photo-three.png', '/widget/photo-one.png']
+  const listText = [
+      ['Оптимизируй работу своей команды', 'Создай свою команду и следим за движениями'],
+      ['Создай свою команду и следим за движениями', 'Оптимизируй работу своей команды'],
+      ['ОпОПОПОПОПОПОП своееее zzzzz', 'Оптимизируй работу своей команды']]
+  const [currentText, setCurrentText] = useState(0)
+  const [currentPicture, setCurrentPicture] = useState(0);
 
   const changePoint = () => {
     const newPoint = [...point];
     const nextIndex = (positionPoint + 1) % point.length;
+
     newPoint[positionPoint] = false;
     newPoint[nextIndex] = true;
+
     setProgress(1)
     setPoint(newPoint);
     setPositionPoint(nextIndex);
+
     setCycleStart(new Date().getTime())
+    changePicture()
+    changeText()
   };
     const intervalFunc = useEffectEvent(() => {
         setTimers(timers + 1)
@@ -29,6 +43,14 @@ export default function Widget() {
 
 
     const changePointAdapter = useEffectEvent(changePoint);
+
+    const changePicture = () => {
+        setOnAnimation(true)
+        setCurrentPicture((currentPicture + 1) % 4 )
+    }
+    const changeText = () => {
+        setCurrentText( (currentText + 1) % 3 )
+    }
 
     const [cycleStart, setCycleStart] = useState(0);
 
@@ -44,6 +66,20 @@ export default function Widget() {
 
     const changeProgressAdapter = useEffectEvent(changeProgress);
 
+    const t = () =>{
+        console.log(currentPicture)
+        if (currentPicture === 3){
+            setOnAnimation(false)
+            setCurrentPicture(0)
+        }
+    }
+    useEffect(() => {
+        const l = setInterval(t, 500)
+        return () => {
+            clearInterval(l)
+        }
+    }, [currentPicture]);
+
     useEffect(() => {
         setCycleStart(new Date().getTime())
         const progressInterval = setInterval(changeProgressAdapter, 50)
@@ -55,20 +91,29 @@ export default function Widget() {
 
 
 
-
+    // <div className={`w-full flex` + onAnimation ? `transition duration-300` :  `transition duration-300` }
     return (
-    <div className="flex w-full h-fullbg-white relative">
-      <div
-        className="widget w-full h-full bg-cover bg-center bg-no-repeat rounded-[40px] flex justify-end items-center"
-        style={{ backgroundImage: "url('/widget/photo-one.png')" }}
-      ></div>
+    <div className="flex w-full h-fullbg-white relative overflow-hidden rounded-[40px]">
+    <div className="w-full flex"
+        ref={elementBlockPicture}
+         style={{transform: `translateX(-${100*currentPicture}%)`, transition: onAnimation ? 'all 0.3s linear' : 'none' }}
+    >
+        {listPicture.map((item, i) => (
+            <div
+                className="shrink-0 widget w-full h-full bg-cover bg-center bg-no-repeat rounded-[40px] flex justify-end items-center "
+                style={{ backgroundImage: `url(${item})` }}
+                ref={ elementPicture }
+                key={ i }
+            />
+        )) }
+    </div>
       <div className="absolute h-full flex w-full bottom-0 items-center justify-end py-[40px] flex-col gap-[34px]">
         <div className="text-center flex flex-col gap-1">
-          <p className="text-[32px] text-white">
-            Оптимизируй работу своей команды
+          <p className="widgetText text-[32px] text-white transition duration-300">
+              {listText[currentText][0]}
           </p>
-          <p className="text-[18px] text-white/70">
-            Создай свою команду и следим за движениями
+          <p className="widgetText text-[18px] text-white/70 transition duration-300">
+              {listText[currentText][1]}
           </p>
         </div>
         <div className="flex gap-[36px]">
