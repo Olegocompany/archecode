4b8d43bbe7433e302de5ccdaae8698ff2d59a23c
rh64s <cchelovekov64@gmail.com>
2026-07-21 14:25:06 +0700

    add: button function

--- frontend/app/shared/ui/button/Button.tsx
+++ frontend/app/shared/ui/button/Button.tsx
@@ -1,28 +1,33 @@
 import { buttonVariants } from "@/app/shared/ui/button/button.variants";
+import { ReactNode } from "react";
 
 interface ButtonProps {
-  handleClick: () => void;
-  text: string;
+  handleClick?: () => void;
   variantButton?: "accent" | "danger" | "solid";
   accessibilityLabel?: string;
   disabled?: boolean;
+  children: ReactNode;
 }
 
 export default function Button({
   handleClick,
-  text,
   variantButton = "accent",
   accessibilityLabel = "",
   disabled = false,
+  children,
 }: ButtonProps) {
+  const handleClickOnButton = () => {
+    if (disabled) return;
+    handleClick?.();
+  };
   return (
     <button
-      onClick={handleClick}
+      onClick={handleClickOnButton}
       disabled={disabled}
       aria-label={accessibilityLabel || undefined}
       className={buttonVariants({ type: variantButton })}
     >
-      {text}
+      {children}
     </button>
   );
 }
