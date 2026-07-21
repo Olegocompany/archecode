3cf165984b9cc92eee6b6faa599c30926ae4cf9b
rh64s <cchelovekov64@gmail.com>
2026-07-21 12:19:08 +0700

    fix: add spread to shadows

--- frontend/app/shared/ui/button/Button.tsx
+++ frontend/app/shared/ui/button/Button.tsx
@@ -1,9 +1,6 @@
-"use client";
-
 import { buttonVariants } from "@/app/shared/ui/button/button.variants";
-import React from "react";
+
 interface ButtonProps {
-  onPress: () => void;
   text: string;
   variantButton?: "accent" | "danger" | "solid";
   accessibilityLabel?: string;
@@ -11,22 +8,16 @@ interface ButtonProps {
 }
 
 function Button({
-  onPress,
   text,
   variantButton = "accent",
   accessibilityLabel = "",
   disabled = false,
 }: ButtonProps) {
-  const handleClick = () => {
-    onPress();
-  };
-
   return (
     <button
       disabled={disabled}
       aria-label={accessibilityLabel || undefined}
       className={buttonVariants({ type: variantButton })}
-      onClick={handleClick}
     >
       {text}
     </button>
