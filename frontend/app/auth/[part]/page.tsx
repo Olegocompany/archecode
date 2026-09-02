0c4c103d84770fa9cb41bebcb8690b16b65e3106
elfi <gugl6369@gmail.com>
2026-09-02 19:46:58 +0700

    add: linear color border

--- frontend/app/auth/[part]/page.tsx
+++ frontend/app/auth/[part]/page.tsx
@@ -2,15 +2,17 @@
 import { ViewTransition } from "react";
 import { usePathname } from "next/navigation";
 import { Widget, Register, Login } from "@/app/auth";
+import styles from "./page.module.css";
 
 export default function Part() {
   const pathname = usePathname();
 
   return (
-    <div className="w-full h-screen bg-white py-[40px] px-[150px] flex items-center justify-center">
+    <div className="w-full h-screen py-[40px] px-[150px] flex items-center justify-center ">
       <div
         className={
-          "grid grid-cols-2 w-full h-full bg-linear-[236deg] from-gray to-dark-gray rounded-[50px] p-[10px] [box-shadow:0_16px_16px_0_rgba(0, 0, 0, 0.45)] relative"
+          ` grid grid-cols-2 w-full h-full rounded-[50px] p-[10px] shadow-[0_16px_16px_0_rgba(0,0,0,0.45)] relative  ` +
+          styles.blockForm
         }
       >
         <Register />
